0cca615dfe9d0a83be3ff41d6e7a16a693753c17
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2020-05-02 20:35:16 +0200

    Fix package-level init

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
-import pvae.distributions
-import pvae.objectives
-import pvae.utils
-import pvae.models
-import pvae.vis
-import pvae.ops
+__all__ = [
+    "distributions",
+    "objectives",
+    "utils",
+    "models",
+    "vis",
+    "ops"
+]
